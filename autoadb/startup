3f1daf5ef2114ad6bb082bc00c731ac16ed0e6de
sevennight2012 <android_star_work@163.com>
2021-05-07 15:28:34 +0800

    添加自动解锁脚本

---
+++ autoadb/startup
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+autoadb scrcpy -s '{}'
---
+++ autoadb/unlock-phone.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+status="$(adb -s 894Y03QW1 shell dumpsys window policy | grep screenState)"
+key="screenState="
+keyLength=${#key}
+status="${status// /}"
+hint=""
+if [[ "SCREEN_STATE_OFF" == "${status:12}" ]]; then
+  adb -s 894Y03QW1 shell input keyevent 26
+  sleep 0.5s
+  adb -s 894Y03QW1 shell input swipe 750 2500 750 500
+  sleep 0.5s
+  adb -s 894Y03QW1 shell input motionevent DOWN 350 1700
+  adb -s 894Y03QW1 shell input motionevent MOVE 1100 1700
+  adb -s 894Y03QW1 shell input motionevent MOVE 1100 2400
+  adb -s 894Y03QW1 shell input motionevent UP 1100 2400
+  hint="屏幕解锁成功"
+else
+  hint="当前屏幕无需解锁"
+fi
+output="{
+  \"items\": [
+    {
+      \"title\": \"$hint\",
+      \"valid\": false
+    }
+  ]
+}"
+echo "$output" | cat
