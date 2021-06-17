c7757605521b4abdb4203da500ccdd3b4a01c855
sevennight2012 <android_star_work@163.com>
2021-06-17 17:15:12 +0800

    更新自动压缩脚本，配合自动操作，将下载下来的图片zip包，移动到fswatch监控的目录下，自动化执行解压缩，并上传TinyPNG进行图片压缩操作

--- autoCompress/doCompress.sh
+++ autoCompress/doCompress.sh
@@ -1,11 +1,40 @@
 #!/bin/sh
 # 这是调用压缩工具的脚本
+source ~/.bash_profile
+result=""
 for i in "$@"; do
+  endFlag="${i: -4}"
   if [ -d $i ]; then
-    echo "$i"
-    result=$(echo $i | java -jar $Tiny_Launcher)
-    # result=$(bash /Users/xuxingchen/temp/fswatch/testJar.sh)
-    result="${result//\"/}"
-    osascript -e "display notification \"$result\" with title \"压缩结果\""
+    if [ "${i%/*}" == "$IMAGE_DIR" ]; then
+      echo ">>>>>> 开始压缩 $i 目录下的图片 <<<<<<"
+      result=$(echo $i | java -jar $Tiny_Launcher)
+      # result=$(bash /Users/xuxingchen/temp/fswatch/testJar.sh)
+      result="${result//\"/}"
+    fi
+  elif [ -f $i ]; then
+    if [[ $i != /* ]]; then
+      result="$i 不是绝对路径"
+      break
+    fi
+    unzipDir=${i%/*}
+    if [[ "$endFlag" != ".zip" ]]; then
+      if [[ "$unzipDir" == "$IMAGE_DIR" ]]; then
+        result="$i 不是zip文件"
+      fi
+      break
+    fi
+    # unzip -n $i -d $unzipDir -x __MACOSX/*
+    hasMacosxDir="$(unzip -l $i | grep "__MACOSX/")"
+    if [[ "$hasMacosxDir" == "" ]]; then
+      # 不是系统压缩的
+      unzip -n $i -d $unzipDir
+    else
+      # 系统压缩包，内部包含 __MACOSX目录
+      unzip -n $i -d $unzipDir -x __MACOSX/*
+    fi
+
   fi
 done
+if [[ "$result" != "" ]]; then
+  osascript -e "display notification \"$result\" with title \"压缩结果\""
+fi
--- autoCompress/watch.sh
+++ autoCompress/watch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source ~/.bash_profile
-fswatch -0 -e ".*_optimized" -e ".*_compress" -e ".DS_Store" -e ".zip" \
-  /Users/xuxingchen/mrcd/img | xargs -0 -n1 -I {} /Users/xuxingchen/.ScriptLauncher/autoCompress/doCompress.sh "{}"
+fswatch -0 -e ".*_optimized" -e ".*_compress" -e ".DS_Store" -e ".*__MACOSX" \
+  $IMAGE_DIR | xargs -0 -n1 -I {} /Users/xuxingchen/.ScriptLauncher/autoCompress/doCompress.sh "{}"
 
 # fswatch -0 /Users/xuxingchen/mrcd/img | while read -d "" event; do
 #   # // do something with ${event}
