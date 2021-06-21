1f432278bca687fa8ad62b31311350d9d3734dbf
sevennight2012 <android_star_work@163.com>
2021-06-21 11:25:04 +0800

    优化autoCompress，自动监控download目录，并且读取新增文件的来源，如果是内网域名的话，那么自动迁移至img目录，并解压缩，然后进行压缩图片的操作，以上操作全部完成后打开对应的目录

--- autoCompress/doCompress.sh
+++ autoCompress/doCompress.sh
@@ -10,6 +10,10 @@ for i in "$@"; do
       result=$(echo $i | java -jar $Tiny_Launcher)
       # result=$(bash /Users/xuxingchen/temp/fswatch/testJar.sh)
       result="${result//\"/}"
+      # 打开目标目录
+      targetDir=${i##*/}_compress
+      echo "打开目录: $IMAGE_DIR/$targetDir"
+      open $IMAGE_DIR/$targetDir
     fi
   elif [ -f $i ]; then
     if [[ $i != /* ]]; then
@@ -23,16 +27,26 @@ for i in "$@"; do
       fi
       break
     fi
-    # unzip -n $i -d $unzipDir -x __MACOSX/*
-    hasMacosxDir="$(unzip -l $i | grep "__MACOSX/")"
-    if [[ "$hasMacosxDir" == "" ]]; then
-      # 不是系统压缩的
-      unzip -n $i -d $unzipDir
+    if [[ "$unzipDir" == "${HOME}/Downloads" ]]; then
+      # 在download目录下面
+      fromUrl="$(mdls -name kMDItemWhereFroms $i | grep "http://office.hinterstellar.com")"
+      echo "$i 文件来源 >>$fromUrl"
+      if [[ "$fromUrl" != "" ]]; then
+        echo "将 $i 移至 $IMAGE_DIR"
+        mv $i $IMAGE_DIR
+      fi
     else
-      # 系统压缩包，内部包含 __MACOSX目录
-      unzip -n $i -d $unzipDir -x __MACOSX/*
+      # 在img目录下面
+      # unzip -n $i -d $unzipDir -x __MACOSX/*
+      hasMacosxDir="$(unzip -l $i | grep "__MACOSX/")"
+      if [[ "$hasMacosxDir" == "" ]]; then
+        # 不是系统压缩的
+        unzip -n $i -d $unzipDir
+      else
+        # 系统压缩包，内部包含 __MACOSX目录
+        unzip -n $i -d $unzipDir -x __MACOSX/*
+      fi
     fi
-
   fi
 done
 if [[ "$result" != "" ]]; then
--- autoCompress/watch.sh
+++ autoCompress/watch.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 source ~/.bash_profile
-fswatch -0 -e ".*_optimized" -e ".*_compress" -e ".DS_Store" -e ".*__MACOSX" \
-  $IMAGE_DIR | xargs -0 -n1 -I {} /Users/xuxingchen/.ScriptLauncher/autoCompress/doCompress.sh "{}"
+fswatch -0 -e ".*_optimized" \
+  -e ".*_compress" \
+  -e ".DS_Store" \
+  -e ".*__MACOSX" \
+  -e "未确认" \
+  $IMAGE_DIR ${HOME}/Downloads | xargs -0 -n1 -I {} /Users/xuxingchen/.ScriptLauncher/autoCompress/doCompress.sh "{}"
 
 # fswatch -0 /Users/xuxingchen/mrcd/img | while read -d "" event; do
 #   # // do something with ${event}
