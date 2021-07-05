2b4ad43ce532e334f14060b48feac3492afcfe82
sevennight2012 <android_star_work@163.com>
2021-07-05 20:31:18 +0800

    更新换新tasker的脚本，支持多设备

--- tasker/openTasker.sh
+++ tasker/openTasker.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 source ~/.bash_profile
-echo ""
-adbResult="$(adb devices | grep 894Y03QW1)"
-if [[ $adbResult != "" ]]; then
-  # 解锁屏幕
-  unlockResult="$(bash $UNLOCK_PATH | grep 屏幕解锁成功)"
-  if [[ $unlockResult != "" ]]; then
-    echo "唤醒屏幕"
-  fi
-  # 休眠1秒后唤醒tasker
-  sleep 1s
-  adb -s 894Y03QW1 shell am start -n net.dinglisch.android.taskerm/.Tasker
-  sleep 3s
-  adb -s 894Y03QW1 shell input keyevent 4
-  adb -s 894Y03QW1 shell input keyevent 4
-  time=$(date "+%Y-%m-%d %H:%M:%S")
-  echo "$time 唤醒tasker成功"
-fi
+# 分别唤醒两部设备上的tasker
+bash ${OPEN_TASKER_SCRIPT_DIR}/open_script.sh 894Y03QW1 &&
+  bash ${OPEN_TASKER_SCRIPT_DIR}/open_script.sh HT7320200007
