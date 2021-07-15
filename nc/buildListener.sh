087aadf88c9ba50d83123b1c69b676ebe374dd58
sevennight2012 <android_star_work@163.com>
2021-07-15 20:44:06 +0800

    add test

--- nc/buildListener.sh
+++ nc/buildListener.sh
@@ -2,9 +2,11 @@
 # while read line; do
 #   echo ">>>>$line"
 # done <"$()"
-nc -lk localhost 12345 | while read line; do
-  echo "$line"
-  if [[ "$line" == "pull" ]]; then
-    bash invoke.sh $line | cat
-  fi
-done
+# nc -lk localhost 12345 | while read line; do
+#   echo "$line"
+#   if [[ "$line" == "pull" ]]; then
+#     result=$(bash test.sh $line)
+#     echo -e "$result \n"
+#   fi
+# done
+nc -lk localhost 12345
