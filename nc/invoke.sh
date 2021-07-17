fbb0fd926f623beef4bbb110d72ec9763b0bd2aa
sevennight2012 <android_star_work@163.com>
2021-07-17 11:00:41 +0800

    调整json临时文件的存储位置，放到~/.ScriptLauncher/nc目录下

--- nc/invoke.sh
+++ nc/invoke.sh
@@ -2,7 +2,7 @@
 if [[ "$1" == "pull" ]]; then
   #statements
   cd ~/mrcd/github/WeShare-Android
-  cat mavenLocal/optimizeProjects.json >mavenLocal/optimizeProjects.json.tmp
+  cat mavenLocal/optimizeProjects.json >~/.ScriptLauncher/nc/optimizeProjects.tmp
   cat ~/.ScriptLauncher/nc/project.json >mavenLocal/optimizeProjects.json
   ./gradlew syncLocalDeps
   result="同步成功"
@@ -10,7 +10,7 @@ if [[ "$1" == "pull" ]]; then
     result="同步失败"
   fi
   osascript -e "display notification \"$result\" with title \"AAR同步任务\""
-  mv mavenLocal/optimizeProjects.json.tmp mavenLocal/optimizeProjects.json
+  mv ~/.ScriptLauncher/nc/optimizeProjects.tmp mavenLocal/optimizeProjects.json
   time=$(date "+%Y-%m-%d %H:%M:%S")
   echo -e "发布 aar 完成 >>>>>> $time \n\n"
   # echo ">>>>>> $(pwd)"
