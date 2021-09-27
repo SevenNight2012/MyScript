8771fc9320640ba6e726b7f4a3f2bf957c181206
xuxingchen <xuxingchen2016@gmail.com>
2021-09-27 20:57:59 +0800

    update compress script

--- autoCompress/doCompress.sh
+++ autoCompress/doCompress.sh
@@ -4,7 +4,8 @@
 # 这样在存储图片的目录下会有两个目录，一个是原图目录，一个是带有_optimized后缀的压缩图目录
 # 存储图片的目录是环境变量 IMAGE_DIR 所定义的
 source ~/.bash_profile
-logFile="$HOME/compress.log"
+# 加载真正的脚本
+source "$COMPRESS_SCRIPT"
 
 # 检查目录的合法性
 function checkDir() {
@@ -47,87 +48,12 @@ function unzipImages() {
   fi
 }
 
-# 专门用来处理图片文件的函数
-function compressImageFile {
-  imageFile="$1"
-  extension="${imageFile##*.}"
-  thisFolder="$2"
-  # 切换目录
-  cd "$thisFolder"
-
-  if [[ "png" == "$extension" || "PNG" == "$extension" ]]; then
-    squoosh-cli \
-    --quant '{"enabled":true,"zx":0,"numColors":64,"dither":0.5}' --oxipng '{"level":2,"interlace":false}'\
-    "$imageFile"\
-    >> "$logFile" 2>&1
-  elif [[ "jpg" == "$extension" || "JPG" == "$extension" || "jpeg" == "$extension" || "JPEG" == "$extension" ]]; then
-    # echo "$imageFile 是jpg文件"
-    squoosh-cli \
-    --mozjpeg '{"quality":75,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":75}'\
-    "$imageFile"\
-    >>"$logFile" 2>&1
-    if [[ "jpeg" == "$extension" || "JPEG" == "$extension" ]]; then
-      # jpeg格式的文件压缩完以后输出文件是jpg的，所以需要手动删除原图
-      rm "$imageFile"
-    fi
-  elif [[ "webp" == "$extension" || "WEBP" == "$extension" ]]; then
-    squoosh-cli \
-    --webp '{"quality":75,"target_size":0,"target_PSNR":0,"method":4,"sns_strength":50,"filter_strength":60,"filter_sharpness":0,"filter_type":1,"partitions":0,"segments":4,"pass":1,"show_compressed":0,"preprocessing":0,"autofilter":0,"partition_limit":0,"alpha_compression":1,"alpha_filtering":1,"alpha_quality":100,"lossless":0,"exact":1,"image_hint":0,"emulate_jpeg_size":0,"thread_level":0,"low_memory":0,"near_lossless":100,"use_delta_palette":0,"use_sharp_yuv":0}'\
-    "$imageFile"\
-    >>"$logFile" 2>&1
-  else
-    echo "$imageFile 不支持 $extension 类型的文件">>"$logFile" 2>&1
-  fi
-}
-
-# 递归遍历文件夹中的文件
-function readFolder() {
-  thisFolder="$1"
-
-  for element in $(ls "$1" | tr " " "\?"); do
-    element=$(tr "\?" " " <<<$element)
-    dir_or_file=$1"/"$element
-    if [ -d "$dir_or_file" ]; then
-      readFolder "$dir_or_file"
-    elif [[ -f "$dir_or_file" ]]; then
-      # 文件
-      # squoosh-cli --quant '{"enabled":true,"zx":0,"numColors":64,"dither":0.6}' --oxipng '{"level":2,"interlace":false}'
-      compressImageFile "$dir_or_file" "$thisFolder"
-    fi
-  done
-}
-
-# 使用Squoosh进行图片压缩
-function compressImageBySquoosh() {
-  imagePath="$1"
-  parentPath="${imagePath%/*}"
-  imageDir="${imagePath##*/}"
-  optimizedDir="$parentPath/${imageDir}_optimized"
-
-  # echo "路径：$imagePath"
-  # echo "父目录：$parentPath"
-  # echo "目录名：$imageDir"
-  # 复制文件夹
-  cp -rf "$imagePath/" "$optimizedDir/"
-  echo "">"$logFile"
-  echo ">>>>>> 开始压缩 "$i" 目录下的图片 <<<<<<">>"$logFile" 2>&1
-  readFolder "$optimizedDir"
-  echo ">>>>>> 压缩结束 <<<<<<">>"$logFile" 2>&1
-  open "$optimizedDir"
-  osascript -e "display notification \"压缩完成\""
-  # open "$logFile"
-  # result="$(cat "$logFile")"
-  # result="$result\n$result"
-  # osascript -e "tell app \"System Events\" to display dialog \"$result\""
-  # osascript -e "do shell script \"open $IMAGE_DIR/$targetDir\""
-}
-
 result=""
 for i in "$@"; do
   endFlag="${i: -4}"
   if [ -d "$i" ]; then
     if [ "${i%/*}" == "$IMAGE_DIR" ]; then
-      compressImageBySquoosh "$i"
+      compressDir "$i"
     fi
   elif [ -f "$i" ]; then
     checkResult=true
--- autoCompress/doCompressBySquoosh.sh
+++ autoCompress/doCompressBySquoosh.sh
@@ -2,6 +2,7 @@
 # 单纯用来压缩图片的脚本，传入目录，则进行拷贝后，递归拷贝的目录压缩图片
 # 传入图片，则直接压缩图片，然后覆盖
 source ~/.bash_profile
+runMain=false
 logFile="$HOME/compress.log"
 
 # 专门用来处理图片文件的函数
@@ -10,15 +11,17 @@ function compressImageFile {
   extension="${imageFile##*.}"
   thisFolder="$2"
   # 切换目录
-  echo "$thisFolder"
+
   cd "$thisFolder"
   if [[ "png" == "$extension" || "PNG" == "$extension" ]]; then
+    # echo "$imageFile 是png"
     squoosh-cli \
     --quant '{"enabled":true,"zx":0,"numColors":64,"dither":0.5}' --oxipng '{"level":2,"interlace":false}'\
     "$imageFile"\
     >> "$logFile" 2>&1
   elif [[ "jpg" == "$extension" || "JPG" == "$extension" || "jpeg" == "$extension" || "JPEG" == "$extension" ]]; then
     # echo "$imageFile 是jpg文件"
+
     squoosh-cli \
     --mozjpeg '{"quality":75,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":75}'\
     "$imageFile"\
@@ -28,6 +31,7 @@ function compressImageFile {
       rm "$imageFile"
     fi
   elif [[ "webp" == "$extension" || "WEBP" == "$extension" ]]; then
+    # echo "$imageFile 是webp"
     squoosh-cli \
     --webp '{"quality":75,"target_size":0,"target_PSNR":0,"method":4,"sns_strength":50,"filter_strength":60,"filter_sharpness":0,"filter_type":1,"partitions":0,"segments":4,"pass":1,"show_compressed":0,"preprocessing":0,"autofilter":0,"partition_limit":0,"alpha_compression":1,"alpha_filtering":1,"alpha_quality":100,"lossless":0,"exact":1,"image_hint":0,"emulate_jpeg_size":0,"thread_level":0,"low_memory":0,"near_lossless":100,"use_delta_palette":0,"use_sharp_yuv":0}'\
     "$imageFile"\
@@ -39,17 +43,17 @@ function compressImageFile {
 
 # 递归遍历文件夹中的文件
 function readFolder() {
-  thisFolder="$1"
 
   for element in $(ls "$1" | tr " " "\?"); do
     element=$(tr "\?" " " <<<$element)
     dir_or_file=$1"/"$element
+    # echo "$dir_or_file  ${dir_or_file%/*}"
     if [ -d "$dir_or_file" ]; then
       readFolder "$dir_or_file"
     elif [[ -f "$dir_or_file" ]]; then
       # 文件
       # squoosh-cli --quant '{"enabled":true,"zx":0,"numColors":64,"dither":0.6}' --oxipng '{"level":2,"interlace":false}'
-      compressImageFile "$dir_or_file" "$thisFolder"
+      compressImageFile "$dir_or_file" "${dir_or_file%/*}"
     fi
   done
 }
@@ -86,5 +90,13 @@ function main() {
     fi
   done
 }
+if [[ $runMain == true ]]; then
+  main "$@"
+fi
 
-main "$@"
+function readInput {
+  params="$(cat | <&0 )"
+  for (( i=0; i<5; i=i+1 )); do
+  echo $i
+  done
+}
