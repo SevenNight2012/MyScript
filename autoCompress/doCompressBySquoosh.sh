4a2052035f1dc62c615e415822c54f5a3aa13f5a
sevennight2012 <android_star_work@163.com>
2021-09-27 23:36:09 +0800

    优化压缩脚本

--- autoCompress/doCompressBySquoosh.sh
+++ autoCompress/doCompressBySquoosh.sh
@@ -6,7 +6,7 @@ runMain=false
 logFile="$HOME/compress.log"
 
 # 专门用来处理图片文件的函数
-function compressImageFile {
+function compressImageFile() {
   imageFile="$1"
   extension="${imageFile##*.}"
   thisFolder="$2"
@@ -16,16 +16,16 @@ function compressImageFile {
   if [[ "png" == "$extension" || "PNG" == "$extension" ]]; then
     # echo "$imageFile 是png"
     squoosh-cli \
-    --quant '{"enabled":true,"zx":0,"numColors":64,"dither":0.5}' --oxipng '{"level":2,"interlace":false}'\
-    "$imageFile"\
-    >> "$logFile" 2>&1
+      --quant '{"enabled":true,"zx":0,"numColors":64,"dither":0.5}' --oxipng '{"level":2,"interlace":false}' \
+      "$imageFile" \
+      >>"$logFile" 2>&1
   elif [[ "jpg" == "$extension" || "JPG" == "$extension" || "jpeg" == "$extension" || "JPEG" == "$extension" ]]; then
     # echo "$imageFile 是jpg文件"
 
     squoosh-cli \
-    --mozjpeg '{"quality":75,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":75}'\
-    "$imageFile"\
-    >>"$logFile" 2>&1
+      --mozjpeg '{"quality":75,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":75}' \
+      "$imageFile" \
+      >>"$logFile" 2>&1
     if [[ "jpeg" == "$extension" || "JPEG" == "$extension" ]]; then
       # jpeg格式的文件压缩完以后输出文件是jpg的，所以需要手动删除原图
       rm "$imageFile"
@@ -33,11 +33,11 @@ function compressImageFile {
   elif [[ "webp" == "$extension" || "WEBP" == "$extension" ]]; then
     # echo "$imageFile 是webp"
     squoosh-cli \
-    --webp '{"quality":75,"target_size":0,"target_PSNR":0,"method":4,"sns_strength":50,"filter_strength":60,"filter_sharpness":0,"filter_type":1,"partitions":0,"segments":4,"pass":1,"show_compressed":0,"preprocessing":0,"autofilter":0,"partition_limit":0,"alpha_compression":1,"alpha_filtering":1,"alpha_quality":100,"lossless":0,"exact":1,"image_hint":0,"emulate_jpeg_size":0,"thread_level":0,"low_memory":0,"near_lossless":100,"use_delta_palette":0,"use_sharp_yuv":0}'\
-    "$imageFile"\
-    >>"$logFile" 2>&1
+      --webp '{"quality":75,"target_size":0,"target_PSNR":0,"method":4,"sns_strength":50,"filter_strength":60,"filter_sharpness":0,"filter_type":1,"partitions":0,"segments":4,"pass":1,"show_compressed":0,"preprocessing":0,"autofilter":0,"partition_limit":0,"alpha_compression":1,"alpha_filtering":1,"alpha_quality":100,"lossless":0,"exact":1,"image_hint":0,"emulate_jpeg_size":0,"thread_level":0,"low_memory":0,"near_lossless":100,"use_delta_palette":0,"use_sharp_yuv":0}' \
+      "$imageFile" \
+      >>"$logFile" 2>&1
   else
-    echo "$imageFile 不支持 $extension 类型的文件">>"$logFile" 2>&1
+    echo "$imageFile 不支持 $extension 类型的文件" >>"$logFile" 2>&1
   fi
 }
 
@@ -70,15 +70,16 @@ function compressDir() {
   # echo "目录名：$imageDir"
   # 复制文件夹
   cp -rf "$imagePath/" "$optimizedDir/"
-  echo "">"$logFile"
-  echo ">>>>>> 开始压缩 "$i" 目录下的图片 <<<<<<">>"$logFile" 2>&1
+  echo ">>>>>> 开始压缩 "$i" 目录下的图片 <<<<<<" >>"$logFile" 2>&1
   readFolder "$optimizedDir"
-  echo ">>>>>> 压缩结束 <<<<<<">>"$logFile" 2>&1
+  echo ">>>>>> 压缩结束 <<<<<<" >>"$logFile" 2>&1
   open "$optimizedDir"
   osascript -e "display notification \"压缩完成\""
 }
 # 主入口
 function main() {
+  echo "" >"$logFile"
+  echo "$@" >"$logFile"
   for i in "$@"; do
     endFlag="${i: -4}"
     if [ -d "$i" ]; then
@@ -94,9 +95,21 @@ if [[ $runMain == true ]]; then
   main "$@"
 fi
 
-function readInput {
-  params="$(cat | <&0 )"
-  for (( i=0; i<5; i=i+1 )); do
-  echo $i
+# 适配自动操作输入参数成2倍的问题
+function readAutomator() {
+  count=$#
+  count=$(($count / 2))
+  echo "" >"$logFile"
+  for ((i = 1; i <= $count; i = i + 1)); do
+    eval img=\${$i}
+    # echo $img >>"$logFile"
+    if [ -d "$img" ]; then
+      # 处理文件夹
+      compressDir "$img"
+    elif [ -f "$img" ]; then
+      # 普通文件
+      compressImageFile "$img" "${img%/*}"
+    fi
   done
+  # echo "finish" >>"$logFile"
 }
