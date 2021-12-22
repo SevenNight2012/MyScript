392f20ebf3db2129d5ab0cbed6446155199fd8d6
xuxingchen <xuxingchen@baidu.com>
2021-12-22 11:14:12 +0800

    add shell.txt

--- .script_profile.sh
+++ .script_profile.sh
@@ -3,5 +3,6 @@ source $SCRIPT_DIR/my_alias.sh
 export HOT_KEY_FILE_DIR=$SCRIPT_DIR/hot_keys
 export OPEN_TASKER_SCRIPT_DIR=$SCRIPT_DIR/tasker
 export COMPRESS_SCRIPT=$SCRIPT_DIR/autoCompress/doCompressBySquoosh.sh
+
 # 本地图片目录，用于自动监控
 export IMAGE_DIR=$HOME/work/img
