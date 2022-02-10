62b14bba035cb6dae587b36dcab812d27a941fac
xuxingchen <xuxingchen@baidu.com>
2022-02-10 20:32:16 +0800

    update openTasker.sh,update unlock telephone shell

--- tasker/894Y03QW1_unlock.sh
+++ tasker/894Y03QW1_unlock.sh
@@ -7,13 +7,13 @@ status="${status// /}"
 hint=""
 if [[ "SCREEN_STATE_OFF" == "${status:12}" ]]; then
   adb -s 894Y03QW1 shell input keyevent 26
-  sleep 0.5s
-  adb -s 894Y03QW1 shell input swipe 750 2500 750 500
-  sleep 0.5s
-  adb -s 894Y03QW1 shell input motionevent DOWN 350 1700
-  adb -s 894Y03QW1 shell input motionevent MOVE 1100 1700
-  adb -s 894Y03QW1 shell input motionevent MOVE 1100 2500
-  adb -s 894Y03QW1 shell input motionevent UP 1100 2500
+  sleep 1
+  adb -s 894Y03QW1 shell input swipe 700 2400 700 500
+  sleep 1
+  adb -s 894Y03QW1 shell input motionevent DOWN 300 1500
+  adb -s 894Y03QW1 shell input motionevent MOVE 1100 1500
+  adb -s 894Y03QW1 shell input motionevent MOVE 1100 2300
+  adb -s 894Y03QW1 shell input motionevent UP 1100 2300
   hint="屏幕解锁成功"
 else
   hint="当前屏幕无需解锁"
